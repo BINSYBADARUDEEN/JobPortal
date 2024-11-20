6ad50e1a888d9aaa1ac1431216371b141114e50a
binsybadarudeen <binsybadarudeen@gmail.com>
2024-11-20 23:37:42 +0000

    apis for creating, updating and fetching jobs - tested and working

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,43 +1,96 @@
-import { jobs } from "../models/jobsSchema.js";
+// import { jobs } from "../models/jobsSchema.js";
 
-export const postJob = function (req, res) {
+// export const postJob = function (req, res) {
     
-    if (!req.body.jobTitle || !req.body.jobId || !req.body.jobDesc || !req.body.jobPostedBy) {
-      console.log(req.body);
-      res.status(400).json({success: false, message: 'Please enter mandatory fields.'});
-    } else {
+//     if (!req.body.jobTitle || !req.body.jobId || !req.body.jobDesc || !req.body.jobPostedBy) {
+//       console.log(req.body);
+//       res.status(400).json({success: false, message: 'Please enter mandatory fields.'});
+//     } else {
       
-        jobs.findOne({ jobId: req.body.jobId }, function (error, resExist) {
-        if (error) {
-          return res.json(error)
-        }
+//         jobs.findOne({ jobId: req.body.jobId }, function (error, resExist) {
+//         if (error) {
+//           return res.json(error)
+//         }
   
-        if(resExist && resExist.jobId) {
-         return res.status(400).json({success: false, message: 'This job already exist by same id.'});
-        }
-        var newJob = new jobs({
-          jobPostedBy: req.body.jobPostedBy,
-          jobTitle:    req.body.jobTitle,
-          jobId:       req.body.jobId,
-          reqExp:      req.body.reqExp,
-          location:    req.body.location,
-          offerSalary: req.body.offerSalary,
-          keySkills:   req.body.keySkills,
-          desiredProf: req.body.desiredProf,
-          jobDesc:     req.body.jobDesc,
-          companyProf: req.body.companyProf,
-          created_at:  new Date(),
-          updated_at:  ""
-        });
+//         if(resExist && resExist.jobId) {
+//          return res.status(400).json({success: false, message: 'This job already exist by same id.'});
+//         }
+//         var newJob = new jobs({
+//           jobPostedBy: req.body.jobPostedBy,
+//           jobTitle:    req.body.jobTitle,
+//           jobId:       req.body.jobId,
+//           reqExp:      req.body.reqExp,
+//           location:    req.body.location,
+//           offerSalary: req.body.offerSalary,
+//           keySkills:   req.body.keySkills,
+//           desiredProf: req.body.desiredProf,
+//           jobDesc:     req.body.jobDesc,
+//           companyProf: req.body.companyProf,
+//           created_at:  new Date(),
+//           updated_at:  ""
+//         });
       
-        newJob.save(function (err,jobRes) {
-          if (err) {
-            return res.json(err)
-          }
-          console.log(req.body);
-          res.status(200).json({success: true, message: 'Successfully created new job.', body:jobRes});
-        });
-      });
+//         newJob.save(function (err,jobRes) {
+//           if (err) {
+//             return res.json(err)
+//           }
+//           console.log(req.body);
+//           res.status(200).json({success: true, message: 'Successfully created new job.', body:jobRes});
+//         });
+//       });
       
-    }
+//     }
+//   }
+const db = require('../database/connection');
+
+// Create a new job
+exports.createJob = (req, res) => {
+  const { title, description, company, location, salary } = req.body;
+  const sql = 'INSERT INTO jobs (title, description, company, location, salary) VALUES (?, ?, ?, ?, ?)';
+  db.query(sql, [title, description, company, location, salary], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).send({ message: 'Job created successfully', jobId: result.insertId });
+  });
+};
+
+// Update a job
+exports.updateJob = (req, res) => {
+  const { id } = req.params;
+  const { title, description, company, location, salary } = req.body;
+  const sql = 'UPDATE jobs SET title = ?, description = ?, company = ?, location = ?, salary = ? WHERE id = ?';
+  db.query(sql, [title, description, company, location, salary, id], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.send({ message: 'Job updated successfully' });
+  });
+};
+
+// Get all jobs
+exports.getAllJobs = (req, res) => {
+  const sql = 'SELECT * FROM jobs';
+  db.query(sql, (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.send(results);
+  });
+};
+
+// Search for jobs by criteria
+exports.searchJob = (req, res) => {
+  const { title, location } = req.query;
+  let sql = 'SELECT * FROM jobs WHERE 1=1';
+  const params = [];
+
+  if (title) {
+    sql += ' AND title LIKE ?';
+    params.push(`%${title}%`);
   }
+
+  if (location) {
+    sql += ' AND location LIKE ?';
+    params.push(`%${location}%`);
+  }
+
+  db.query(sql, params, (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.send(results);
+  });
+};
--- routes/job.js
+++ routes/job.js
@@ -1,23 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var jobController = require('../controllers/jobController');
+const express = require('express');
+const router = express.Router();
+const jobController =  require('../controllers/jobController');
 
-//api for create job
-router.route('/v1/job/createJob').post(jobController.postJob);
+// Create a new job
+router.post('/jobs', jobController.createJob);
 
-//api for getting the all jobs posted
-router.route('/v1/job/getAllJob').get(jobController.getAllJob);
+// Update a job
+router.put('/jobs/:id', jobController.updateJob);
 
-//api for getting the job data from his id
-router.route('/v1/job/getJob/:id').get(jobController.getJob);
+// Get all jobs
+router.get('/jobs', jobController.getAllJobs);
 
-//api for searching the job data by JobTitle keyword
-router.route('/v1/job/searchJob').get(jobController.searchJob);
+// Search for jobs by criteria
+router.get('/jobs/search', jobController.searchJob);
 
-//api for updating the data of the job
-router.route('/v1/job/updateJob/:jobId').put(jobController.updateJob);
-
-//api for deleting the job
-router.route('/v1/job/deleteJob/:jobId').delete(jobController.deleteJob);
 
 module.exports = router;
