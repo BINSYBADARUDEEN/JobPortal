b42599fa429f08063134207368247f1b8d5f00ae
binsybadarudeen <binsybadarudeen@gmail.com>
2024-12-03 23:12:37 +0000

    added cors

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const jobRoutes = require('./routes/job');
 const db = require('./database/connection');
 const app = express();
+const cors = require('cors');
 
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+}));
 app.use(express.json());
 app.use('/api', jobRoutes);
 
