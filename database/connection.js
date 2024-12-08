8204efe69fd090d4124dceab19278ac83c7f99e8
binsybadarudeen <binsybadarudeen@gmail.com>
2024-12-08 16:47:17 +0000

    changed db details for vm

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 const db = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'20036803',
+    password:'',
     database:'jobPortal'
 });
 db.connect((error) => {
