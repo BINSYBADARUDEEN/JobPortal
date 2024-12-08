8fbc998cc84baa0962a589027ede7ecae2c6c9ed
binsybadarudeen <binsybadarudeen@gmail.com>
2024-12-08 15:18:31 +0000

    added unit test for successfully creating a job

--- test/controllers/jobController.test.js
+++ test/controllers/jobController.test.js
@@ -8,3 +8,37 @@ jest.mock('../../database/connection', () => ({
 }));
 
 const db = require('../../database/connection');
+const app = express();
+app.use(express.json());
+//mocking the routes for test
+app.post('/jobs', jobController.createJob);
+app.put('/jobs/:id', jobController.updateJob);
+
+describe('Job Controller', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    describe('createJob', () => {
+        it('create a new job', async () => {
+            const mockResult = { insertId: 1 };
+            db.query.mockImplementation((sql, params, callback) => callback(null, mockResult));
+            const response = await request(app)
+                .post('/jobs')
+                .send({
+                    title: 'Kitchen Potter',
+                    description: '6 months experience mandatory',
+                    company: 'Zam Zam Cafe',
+                    location: 'Kerala',
+                    salary: 80000,
+                    email: 'zamzamcafe@gmail.com',
+                });
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual({ message: 'Job created successfully', jobId: 1 });
+            expect(db.query).toHaveBeenCalledWith(
+                expect.stringContaining('INSERT INTO jobs'),
+                expect.any(Array),
+                expect.any(Function)
+            );
+        });
+    });
+});
