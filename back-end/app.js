d1aef0825fb02199571c292c58586a6a3bf4d9a1
binsybadarudeen <binsybadarudeen@gmail.com>
2024-12-12 04:37:15 +0000

    added login route

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jobRoutes = require('./routes/job');
+const loginRoutes = require('./routes/login');
 const db = require('./database/connection');
 const app = express();
 const cors = require('cors');
@@ -10,7 +11,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/api', jobRoutes);
-
+app.use('/api', loginRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
