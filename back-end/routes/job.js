1bc46ee335cc84adfd34853fa99bc96381ee6db6
binsybadarudeen <binsybadarudeen@gmail.com>
2024-12-12 04:50:17 +0000

    role based access to apis

--- back-end/routes/job.js
+++ back-end/routes/job.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 var jobController = require('../controllers/jobController');
 
+const checkAdmin = (req, res, next) => {
+    const { role } = req.body; 
+    if (role !== 'admin') return res.status(403).send({ message: 'Access denied' });
+    next();
+};
+
+
 //api for create job
-router.post('/jobs', jobController.createJob);
+router.post('/jobs',checkAdmin, jobController.createJob);
 
 //api for update job
 router.put('/jobs/:id', jobController.updateJob);
@@ -15,7 +22,7 @@ router.get('/jobs', jobController.getAllJobs);
 router.get('/jobs/search', jobController.searchJob);
 
 //api for deleting the job
-router.delete('/jobs/:id', jobController.deleteJob);
+router.delete('/jobs/:id',checkAdmin, jobController.deleteJob);
 
 //api for getting job with id
 router.get('/jobs/:id', jobController.getJobById);
