0b6f86355f6abac9951758377d58d26d94f064f1
binsybadarudeen <binsybadarudeen@gmail.com>
2024-12-12 04:44:52 +0000

    fetch from db for login

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -3,17 +3,13 @@ exports.login = (req, res) => {
     const { username, password } = req.body;
     const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
     db.query(sql, [username, password], (err, results) => {
-        if (err) {
-            console.error('Database error:', err);
-            return res.status(500).send({ message: 'Internal Server error' });
-        }
         if (results.length === 0) {
             return res.status(401).send({ message: 'Invalid username or password' });
         }
         const user = results[0];
         res.send({
             message: 'Login successful',
-            user: { username: user.username}
+            user: { id: user.id, username: user.username, role: user.role}
         });
     });
 };
