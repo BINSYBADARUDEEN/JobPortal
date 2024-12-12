a27c69737d8bfdfd05cb8d9ef203b4ffaf187cc7
binsybadarudeen <binsybadarudeen@gmail.com>
2024-12-12 01:38:07 +0000

    added login method to check username and password

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,1 +1,19 @@
 const db = require('../database/connection');
+exports.login = (req, res) => {
+    const { username, password } = req.body;
+    const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    db.query(sql, [username, password], (err, results) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).send({ message: 'Internal Server error' });
+        }
+        if (results.length === 0) {
+            return res.status(401).send({ message: 'Invalid username or password' });
+        }
+        const user = results[0];
+        res.send({
+            message: 'Login successful',
+            user: { username: user.username}
+        });
+    });
+};
